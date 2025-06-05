ff07ab28f005098e14799fcb1298dbeddec6e225
Kattale256 <kattale.gh@gmail.com>
2025-06-05 11:52:31 +0300

    Update capacitor.config.ts
    Self containing the APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,17 +3,16 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.e61af74b662a4dfca62c40913fe1ff69',
-  appName: 'D1 Project, LDC KAMPALA (C) 2025',
+  appName: 'AKAMEME TAX APP',
   webDir: 'dist',
-  server: {
-    url: "https://e61af74b-662a-4dfc-a62c-40913fe1ff69.lovableproject.com?forceHideBadge=true",
-    cleartext: true
-  },
+  // Remove or comment out this section for production builds:
+  // server: {
+  //   url: "https://e61af74b-662a-4dfc-a62c-40913fe1ff69.lovableproject.com?forceHideBadge=true",
+  //   cleartext: true
+  // },
   plugins: {
     Permissions: {
       sms: true
     }
   }
 };
-
-export default config;
